929d5994fa8404593137401a77c743dae8d7764f
Sagar Kumar <sagarkr2176@gmail.com>
2023-12-19 10:05:14 +0530

    Made Bottom Navigation Bar

--- app/src/main/java/com/example/multimediahub/MainActivity.kt
+++ app/src/main/java/com/example/multimediahub/MainActivity.kt
@@ -2,11 +2,78 @@ package com.example.multimediahub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.example.multimediahub.Adapter.AdapterViewPager
+import com.example.multimediahub.fragment.FragmentAudio
+import com.example.multimediahub.fragment.FragmentImages
+import com.example.multimediahub.fragment.FragmentPDFs
+import com.example.multimediahub.fragment.FragmentVideos
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
+    private var pagerMain: ViewPager2? = null
+    private var fragmentArrayList = ArrayList<Fragment>()
+    var bottomNav: BottomNavigationView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        pagerMain = findViewById(R.id.pagerMain)
+        bottomNav = findViewById(R.id.bottomNav)
+        fragmentArrayList.add(FragmentPDFs())
+        fragmentArrayList.add(FragmentImages())
+        fragmentArrayList.add(FragmentVideos())
+        fragmentArrayList.add(FragmentAudio())
+        val adapterViewPager = AdapterViewPager(this, fragmentArrayList)
+        //setAdapter
+        pagerMain?.run {
+            adapter = adapterViewPager
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    when (position) {
+                        0 -> {
+                            bottomNav?.run { selectedItemId = R.id.itPDFs }
+                        }
+
+                        1 -> {
+                            bottomNav?.run { selectedItemId = R.id.itImages }
+                        }
+
+                        2 -> {
+                            bottomNav?.run { selectedItemId = R.id.itVideos }
+                        }
+
+                        3 -> {
+                            bottomNav?.run { selectedItemId = R.id.itAudio }
+                        }
+                    }
+                    super.onPageSelected(position)
+                }
+            })
+        }
+        bottomNav.run {
+            this?.setOnItemSelectedListener(NavigationBarView.OnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.itPDFs -> {
+                        pagerMain?.run { currentItem = 0 }
+                    }
+
+                    R.id.itImages -> {
+                        pagerMain?.run { currentItem = 1 }
+                    }
+
+                    R.id.itVideos -> {
+                        pagerMain?.run { currentItem = 2 }
+                    }
+
+                    R.id.itAudio -> {
+                        pagerMain?.run { currentItem = 3 }
+                    }
+                }
+                true
+            })
+        }
     }
 }
